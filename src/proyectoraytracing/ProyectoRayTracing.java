213d8cacf640f463e1d52a18114f703de6a3f044
maurihv13 <huaytamauricio1@gmail.com>
2021-12-17 15:06:36 -0400

    Agregando escritura de imagen

--- src/proyectoraytracing/ProyectoRayTracing.java
+++ src/proyectoraytracing/ProyectoRayTracing.java
@@ -5,6 +5,11 @@
  */
 package proyectoraytracing;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author MAURICIO
@@ -15,7 +20,37 @@ public class ProyectoRayTracing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        long start = System.nanoTime();
+        
+        int height, width;
+        height = 900;
+        width = 1600;
+        
+        BufferedImage buffer;
+        File image;
+        
+        String filename = "Image.png";
+        image = new File(filename);
+        buffer = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+    
+        
+        for(int y=0;y < height ; y++){
+            for(int x=0; x < width ; x++){
+                
+            }
+        }
+        
+        //Escribe la imagen
+        try{
+            ImageIO.write(buffer, "PNG", image);
+        }catch(IOException e){
+            System.out.println("Could not write image");
+            System.exit(1);
+        }
+        
+        
+        long end = System.nanoTime();
+        System.out.println("Loop Time: "+ (end-start)/1000000000.0F);
     }
     
 }
