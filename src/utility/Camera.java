5b99b83dffaa5404c7551a74a3748bbf4a2aef71
maurihv13 <huaytamauricio1@gmail.com>
2021-12-17 15:37:53 -0400

    Agregado de camara

--- src/utility/Camera.java
+++ src/utility/Camera.java
@@ -10,5 +10,19 @@ package utility;
  * @author MAURICIO
  */
 public class Camera {
+    public Vector3D campos, camdir, camrigth, camdown;
     
+    public Camera(){
+        campos = new Vector3D(0, 0, 0);
+        camdir = new Vector3D(0, 0, 1);
+        camrigth = new Vector3D(0, 0, 0);
+        camdown = new Vector3D(0, 0, 0);
+    }
+    
+    public Camera(Vector3D pos, Vector3D dir, Vector3D rigth, Vector3D down){
+        campos = pos;
+        camdir = dir;
+        camrigth = rigth;
+        camdown = down;
+    }
 }
